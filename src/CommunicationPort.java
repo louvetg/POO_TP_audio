f79ef67c03f1eb0dd730cf037beefc2a9e93621c
louvetg <louvetg@D9020M25311.phelma.grenoble-inp.fr>
2017-03-29 15:40:26 +0200

    fin première séance

--- src/CommunicationPort.java
+++ src/CommunicationPort.java
@@ -1,8 +1,34 @@
 public class CommunicationPort {
 	private ModuleAbstract possesseur;
-	private int number;
+	private int portNumber;
 	private Connexion connexion;
 	private double value;
 	
+	public CommunicationPort(ModuleAbstract possesseur, int portNumber){
+		this.possesseur = possesseur;
+		this.portNumber = portNumber;
+		this.connexion = null;
+		this.value = 0;
+	}
 	
+	public void setValue(double v){
+		this.value = v;
+	}
+	
+	public double getvalue(){
+		return this.value;
+	}
+	
+	public boolean isConnected(){
+		if(this.connexion != null){return true;}
+		else{return false;}
+	}
+	
+	public void setConnexion(Connexion c){
+		this.connexion = c;
+	}
+	
+	public Connexion getConnexion(){
+		return this.connexion;
+	}
 }
